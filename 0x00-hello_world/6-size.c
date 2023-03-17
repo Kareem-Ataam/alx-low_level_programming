3c4360fceaab067391792f40c5daca40496bfd5f
Kareem-Ataam <kareem.ataam12345@gmail.com>
2023-03-17 04:41:32 +0200

    Get sizes of data types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,10 +7,11 @@
 int main(void)
 {
 printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(char));
-printf("Size of an int: %ld byte(s)\n", (unsigned long)sizeof(int));
-printf("Size of a long int: %ld byte(s)\n", (unsigned long)sizeof(long));
-printf("Size of a long long: %ld byte(s)\n", (unsigned long)sizeof(long long));
-printf("Size of a float: %ld byte(s)\n", (unsigned long)sizeof(float));
+printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(int));
+printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(long));
+printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof
+(long long));
+printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(float));
 return (0);
 }
 
