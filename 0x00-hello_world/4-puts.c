3017c37f3f64e2df70f31274878950442d742d25
Kareem-Ataam <kareem.ataam12345@gmail.com>
2023-03-17 03:56:08 +0200

    Print a line using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+* main- A program prints Programming is like building a
+* multilingual puzzle to STDOUT
+* Return: 0 (Sucess)
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\"\n");
+	return (0);
+}
