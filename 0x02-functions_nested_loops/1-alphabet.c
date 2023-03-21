3f81c619c5759340702cdcaf3a1b5eebe8d9126f
Kareem-Ataam <kareem.ataam12345@gmail.com>
2023-03-22 01:26:52 +0200

    Print the alphabets without using builtin functions

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,5 @@
 #include "main.h"
 /**
-* main - Prints the alphbets in lowercase
-* Return: 0 (Success)
-*/
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
-
-/**
 * print_alphabet - Print the alphabets in lower case
 * Without using builtin function
 */
