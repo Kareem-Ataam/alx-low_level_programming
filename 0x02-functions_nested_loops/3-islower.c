6352b98b980f81b65086d3357fa50a818e7f0984
Kareem-Ataam <kareem.ataam12345@gmail.com>
2023-03-22 01:55:42 +0200

    Check if a character in lower or upper case

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include "ctype"
+/**
+* _islower - Check if a given character is
+* in upper or lower case
+* @c: The character that will be checked
+*
+* Return: 1 if the character is in lowercase
+* and 0 if the character is in uppercase
+*/
+int _islower(int c)
+{
+	if (islower(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
+
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,3 +1,4 @@
 0-putchar.c: Print the word _putchar without using builtin function for printing
 1-alphabet.c:Print the alphabets without using builtin functions
 2-print_alphabet_x10.c:Print alphabets 10 times
+3-islower.c:Check if a character in lower or upper case
