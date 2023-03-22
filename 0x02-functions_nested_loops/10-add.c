94a4d760aa41f5cb56fde245cdee230bb1c75e0b
Kareem-Ataam <kareem.ataam12345@gmail.com>
2023-03-22 04:13:15 +0200

    Add two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+* add - Add two numbers
+* @a: The first number
+* @b: The second number
+*
+* Return: The result of the addition
+*/
+int add(int a, int b)
+{
+	return (a + b);
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -8,3 +8,4 @@
 7-print_last_digit.c:Print the last digit of a number
 8-24_hours.c:Print every minute of the day starting from 00:00 to 23:59
 9-times_table.c:Print a table
+10-add.c:Add two numbers
