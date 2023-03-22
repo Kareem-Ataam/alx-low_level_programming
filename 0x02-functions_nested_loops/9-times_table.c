4b5e44afd4ede1c527243c8de9dfdad62a0b2fa3
Kareem-Ataam <kareem.ataam12345@gmail.com>
2023-03-22 03:41:29 +0200

    Print a table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -4,13 +4,13 @@
 */
 void times_table(void)
 {
-	int i,j,plus = 0;
+	int i, j;
 
-	for (i = 0;i<10;i++)
+	for (i = 0 ; i < 10 ; i++)
 	{
-		for(j = 0;j<10;j++)
+		for (j = 0 ; j < 10 ; j++)
 		{
-			_putchar((i*j) + '0');
+			_putchar((i * j) + '0');
 		}
 		_putchar('\n');
 	}
