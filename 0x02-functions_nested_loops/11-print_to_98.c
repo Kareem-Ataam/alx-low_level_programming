b289e3b2396afaebcaa8d853cb50a2da736dea37
Kareem-Ataam <kareem.ataam12345@gmail.com>
2023-03-22 04:38:29 +0200

    Print a set of natural numbers

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,3 +1,4 @@
+#include<stdio.h>
 #include "main.h"
 /**
 * print_to_98 - Print the set of numbers from
@@ -11,11 +12,26 @@ void print_to_98(int n)
 	if (n <= 98)
 	{
 		for (i = n; i <= 98 ; i++)
-			printf("%d", i);
+		{
+			if (i == 98)
+			{
+				printf("%d", i);
+				continue;
+			}
+			printf("%d, ", i);
+		}
+
 	}
 	else
 	{
 		for (i = n ; i >= 98 ; i--)
-			printf("%d", i);
+		{
+			if (i == 98)
+			{
+				printf("%d", i);
+				continue;
+			}
+			printf("%d, ", i);
+		}
 	}
 }
