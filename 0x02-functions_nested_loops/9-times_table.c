91ece9bd8edfb7a2591d39b80540b3eb21750a66
Kareem-Ataam <kareem.ataam12345@gmail.com>
2023-03-22 03:37:15 +0200

    Print a table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* times_table - Print a table of numbers of i*j
+*/
+void times_table(void)
+{
+	int i,j,plus = 0;
+
+	for (i = 0;i<10;i++)
+	{
+		for(j = 0;j<10;j++)
+		{
+			_putchar((i*j) + '0');
+		}
+		_putchar('\n');
+	}
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -6,3 +6,5 @@
 5-sign.c:Print the sign of a given number
 6-abs.c:Get the absolute of a number
 7-print_last_digit.c:Print the last digit of a number
+8-24_hours.c:Print every minute of the day starting from 00:00 to 23:59
+9-times_table.c:Print a table
