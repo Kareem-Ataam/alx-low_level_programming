f475e2a0baaf68b5ab3ab4c6280fd36c144f0285
Kareem-Ataam <kareem.ataam12345@gmail.com>
2023-03-23 22:13:28 +0200

    Print form 0 to 9 excluding 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* print_most_numbers - Print the numbers from 0 to 9
+* excluding 2 and 4
+*/
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if (i == 2 || i == 4)
+		{
+			continue;
+		}
+		_putchar(i + '0');
+	}
+	_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -2,3 +2,4 @@
 1-isdigit.c:Check is a given character is a digit
 2-mul.c:Multiply two numbers
 3-print_numbers.c:Print from 0 to 9
+4-print_most_numbers.c:Print form 0 to 9 excluding 2 and 4
