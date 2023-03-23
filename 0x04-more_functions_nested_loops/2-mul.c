ed0193361539a6ba959c56361377c472b936f232
Kareem-Ataam <kareem.ataam12345@gmail.com>
2023-03-23 21:06:22 +0200

    Multiply two numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+/**
+* mul - Multiply two numbers
+* @a: The first number
+* @b: The second number
+*
+* Return: The result of the multiplication
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -1,2 +1,3 @@
 0-isupper.c:Check if a character is in upper or lower case
 1-isdigit.c:Check is a given character is a digit
+2-mul.c:Multiply two numbers
