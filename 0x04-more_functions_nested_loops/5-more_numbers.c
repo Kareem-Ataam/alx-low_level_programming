db15baaf23b60a5958d9b8b14f589dbe30d8a71a
Kareem-Ataam <kareem.ataam12345@gmail.com>
2023-03-24 02:41:55 +0200

    Print the numbers from 0 to 14 10 times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* more_numbers - Print 10 times the numbers from 0-14
+*/
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			_putchar(j + '0');
+		}
+		_putchar('\n');
+	}
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -3,3 +3,4 @@
 2-mul.c:Multiply two numbers
 3-print_numbers.c:Print from 0 to 9
 4-print_most_numbers.c:Print form 0 to 9 excluding 2 and 4
+5-more_numbers.c :Print the numbers from 0 to 14 10 times 
