0f25bf03f3e63a259572bae57e8cfa435848235e
Kareem-Ataam <kareem.ataam12345@gmail.com>
2023-03-28 01:51:58 +0200

    Revrese a string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,8 +7,9 @@ void rev_string(char *s)
 {
 	int j = 0;
 	char temp;
+	int i;
 
-	for (int i = strlen(s) - 1; i >= strlen(s) / 2; i--)
+	for (i = strlen(s) - 1; i >= strlen(s) / 2; i--)
 	{
 		temp = s[j];
 		s[j] = s[i];
