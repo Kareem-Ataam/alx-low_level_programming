e424a4d42eedac6d2aa94af820dc4692a4d455f8
Kareem-Ataam <kareem.ataam12345@gmail.com>
2023-03-27 23:41:49 +0200

    Get the length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,10 @@
+#include<string.h>
+/**
+ * _strlen - Get the length of a string
+ * @s: The string to work on
+ * Return: The length of the string
+ */
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
--- 0x05-pointers_arrays_strings/README.md
+++ 0x05-pointers_arrays_strings/README.md
@@ -1,2 +1,3 @@
 0-reset_to_98.c:takes a pointer to an int as parameter and updates the value it points to to 98
 1-swap.c:Swap two varaibles
+2-strlen.c:Get the length of a string
