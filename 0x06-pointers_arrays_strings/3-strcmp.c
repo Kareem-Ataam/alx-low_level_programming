636600d8f1c1085aeb02207fc6d664d85bac99ce
Kareem-Ataam <kareem.ataam12345@gmail.com>
2023-03-30 03:15:10 +0200

    Compare two strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,14 @@
+#include<string.h>
+/**
+ * _strcmp - Compare two strings
+ * @s1: The first string
+ * @s2: The second string
+ *
+ * Return: (0) if they are equal
+ * negative number if the first is less than the second
+ * Positive number if the second is less than the first
+ */
+int _strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1, s2));
+}
--- 0x06-pointers_arrays_strings/README.md
+++ 0x06-pointers_arrays_strings/README.md
@@ -1,3 +1,4 @@
 0-strcat.c:Cocatnate two strings
 1-strncat.c:Concatnate limited number of charcters
 2-strncpy.c:Copy limited number of characters
+3-strcmp.c:Compare two strings
