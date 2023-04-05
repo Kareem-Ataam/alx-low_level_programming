a5bc959894b778c9584b94f27cde4a5e207bd462
Kareem-Ataam <kareem.ataam12345@gmail.com>
2023-04-05 22:33:21 +0200

    Calculate the factorial using recursion

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,14 @@
+#include"main.h"
+/**
+ * factorial - Calculate the factorial using recursion
+ * @n: The number to calcuate the factorial for
+ * Return: The factorial of the number
+ */
+int factorial(int n)
+{
+	if (n == 1)
+	{
+		return (1);
+	}
+	return (n * factorial(n - 1));
+}
