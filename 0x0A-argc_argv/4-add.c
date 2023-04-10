1a01f5c9d8db0068f55271346e28c0ca0d9b5cd5
Kareem-Ataam <kareem.ataam12345@gmail.com>
2023-04-10 20:58:18 +0200

    Add the numbers passed to the program through CLI

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,5 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<ctype.h>
+#include<string.h>
 /**
  * main - Add the numbers passed to the program and print them
  * @argc: Number of arguments passed on the command line
@@ -13,9 +15,12 @@ int main(int argc, char **argv)
 	sum = 0;
 	for (i = 1; i < argc; i++)
 	{
-		if (atoi(argv[i]))
+		if (strcmp(argv[i], "0") == 0 || atoi(argv[i]))
 		{
-			sum += atoi(argv[i]);
+			if (atoi(argv[i]) >= 0)
+			{
+				sum += atoi(argv[i]);
+			}
 		}
 		else
 		{
