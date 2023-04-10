bd02ccb2751a471f3f8fd59be561a8ddd58a0680
Kareem-Ataam <kareem.ataam12345@gmail.com>
2023-04-10 19:26:53 +0200

    Print the name of the program

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ * main - Print the name of the program
+ * @argc: Number of arguments passed on the command line
+ * @argv: argument vector representing the arguments
+ * Return: (0) on sucess
+ */
+int main(int argc, char **argv)
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+0-whatsmyname.c:Program that prints its name
