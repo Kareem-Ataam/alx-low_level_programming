5cf85447712a7cfb242640080bf33ceb6d7d1c42
Kareem-Ataam <kareem.ataam12345@gmail.com>
2023-04-10 21:35:42 +0200

    Add the numbers passed to the program through CLI

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -3,6 +3,21 @@
 #include<ctype.h>
 #include<string.h>
 /**
+ * check_number - check if the argument passed can be converted to a number
+ * @str: Represent the arguments that will be passed
+ * Return: (0) if the argument can't be converted to a number (1) otherwise
+ */
+int check_number(char *str)
+{
+	while (*str != '\0')
+	{
+		if (!isdigit(*str))
+			return (0);
+		str++;
+	}
+	return (1);
+}
+/**
  * main - Add the numbers passed to the program and print them
  * @argc: Number of arguments passed on the command line
  * @argv: argument vector representing the arguments
@@ -15,7 +30,7 @@ int main(int argc, char **argv)
 	sum = 0;
 	for (i = 1; i < argc; i++)
 	{
-		if (strcmp(argv[i], "0") == 0 || atoi(argv[i]))
+		if (check_number(argv[i]))
 		{
 			if (atoi(argv[i]) >= 0)
 			{
