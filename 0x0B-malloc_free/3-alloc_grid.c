d5e1afbd5bfc01fbaeb640fa70d6ec2d34bf795a
Kareem-Ataam <kareem.ataam12345@gmail.com>
2023-04-12 01:49:43 +0200

    Create a 2d array in the Heap

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,41 @@
+#include<stdlib.h>
+/**
+ * alloc_grid - Create a 2d array in the Heap
+ * @width: Number of columns
+ * @height: Number of rows
+ *
+ * Return: Pointer to the 2d array
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int i, j;
+	int **arr;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+	arr = malloc(height * sizeof(int *));
+	if (arr == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < height; i++)
+	{
+		arr[i] = malloc(width * sizeof(int));
+
+		if (arr[i] == NULL)
+		{
+			return (NULL);
+		}
+	}
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			arr[i][j] = 0;
+		}
+	}
+	return (arr);
+}
--- 0x0B-malloc_free/README.md
+++ 0x0B-malloc_free/README.md
@@ -1,3 +1,4 @@
 0-create_array.c:Allocate an array oc characters in the Heap and intilize them with a character
 1-strdup.c:Allocate a space in the Heap for a string
 2-str_concat.c:Create a function to concate two strings
+3-alloc_grid.c:Create a 2d array in the Heap
