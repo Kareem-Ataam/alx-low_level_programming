7414227b21edc8a529756c4b4790a42dc3f4ef0b
Kareem-Ataam <kareem.ataam12345@gmail.com>
2023-04-11 23:56:46 +0200

    Allocate an array oc characters in the Heap and intilize them with a character

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,28 @@
+#include<stdlib.h>
+#include"main.h"
+/**
+ * create_array - Allocate a number of bytes in the heap
+ * @size: number of bytes to be allocated
+ * @c: The character to intialize allocated locations with
+ * Return: Pointer the allocated block
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *ptr;
+	int i;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	ptr = (char *) malloc(size);
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		ptr[i] = c;
+	}
+	return (ptr);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,1 @@
+0-create_array.c:Allocate an array oc characters in the Heap and intilize them with a character
