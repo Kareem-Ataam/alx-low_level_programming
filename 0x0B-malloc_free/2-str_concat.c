83e7fdc1edffe83560ea2a43323ab75e5fbb5a6d
Kareem-Ataam <kareem.ataam12345@gmail.com>
2023-04-12 00:41:36 +0200

    Concate two strings

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,35 @@
+#include<string.h>
+#include<stdlib.h>
+/**
+ * str_concat - Concate two strings
+ * @s1: The first string
+ * @s2: The second string
+ *
+ * Return: Pointer to the result
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *ptr;
+	size_t i, j;
+	size_t len1, len2;
+
+	len1 = (s1 == NULL) ? 0 : strlen(s1);
+	len2 = (s2 == NULL) ? 0 : strlen(s2);
+	j = 0;
+	if (s1 == NULL && s2 == NULL)
+	{
+		return (NULL);
+	}
+	ptr = malloc(len1 + len2 + 1);
+	for (i = 0; i < len1; i++)
+	{
+		ptr[i] = s1[i];
+	}
+	for (i = strlen(s1); i < len1 + len2; i++, j++)
+	{
+		ptr[i] = s2[j];
+	}
+	ptr[len1 + len2] = '\0';
+
+	return (ptr);
+}
--- 0x0B-malloc_free/README.md
+++ 0x0B-malloc_free/README.md
@@ -1,2 +1,3 @@
 0-create_array.c:Allocate an array oc characters in the Heap and intilize them with a character
 1-strdup.c:Allocate a space in the Heap for a string
+2-str_concat.c:Create a function to concate two strings
