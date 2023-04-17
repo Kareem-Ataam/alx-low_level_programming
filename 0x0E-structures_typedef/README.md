e518a9ca9ac417615e51bccee586a8857423e4cc
Kareem-Ataam <kareem.ataam12345@gmail.com>
2023-04-18 01:06:31 +0200

    Define a new type struct dog

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+dog.h:Define a new type struct dog
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+
+#ifndef _HEADER_H
+#define _HEADER_H
+/**
+ * struct dog - dog structure that combine three data items
+ * @name: The name of the dog
+ * @age: The age of the dog
+ * @owner: The name of the owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
