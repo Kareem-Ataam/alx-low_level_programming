78cd21d9c11fc3fe2a8c3a7c8d1929d7ba7258d2
Kareem-Ataam <kareem.ataam12345@gmail.com>
2023-04-18 02:27:59 +0200

    New name for struct dog as dog_t

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,4 +15,8 @@ struct dog
 };
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+/**
+ * dog_t - new name for struct dog
+ */
+typedef struct dog dog_t;
 #endif
