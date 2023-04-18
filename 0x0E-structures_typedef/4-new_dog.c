8dfe4b9a2aa2cfffef18e7fccb62447706938980
Kareem-Ataam <kareem.ataam12345@gmail.com>
2023-04-18 02:57:07 +0200

    Create a new struct dog variable

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,6 +12,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *ptr;
 
+	if (name == NULL || owner == NULL || age < 0)
+	{
+		return (NULL);
+	}
 	ptr = malloc(sizeof(*ptr));
 	if (!ptr)
 	{
