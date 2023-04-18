d459bf96ed57c9f62644dec3c78b3d0b0609436b
Kareem-Ataam <kareem.ataam12345@gmail.com>
2023-04-18 02:43:38 +0200

    Create a new struct dog variable

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,9 +15,9 @@ struct dog
 };
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
 /**
  * dog_t - new name for struct dog
  */
 typedef struct dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
 #endif
