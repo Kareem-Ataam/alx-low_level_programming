1782378da739d02227c3350cfb438deb0dc1c449
Kareem-Ataam <kareem.ataam12345@gmail.com>
2023-04-18 02:40:15 +0200

    Create a new struct dog variable

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,25 @@
+#include<stdlib.h>
+#include"dog.h"
+/**
+ * new_dog - create a new dog_t variable
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
+ *
+ * Return: Pointer to the variable (Success), NULL on failure
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *ptr;
+
+	ptr = malloc(sizeof(*ptr));
+	if (!ptr)
+	{
+		return (NULL);
+	}
+
+	ptr->name = name;
+	ptr->age = age;
+	ptr->owner = owner;
+	return (ptr);
+}
--- 0x0E-structures_typedef/README.md
+++ 0x0E-structures_typedef/README.md
@@ -1,3 +1,4 @@
 dog.h:Define a new type struct dog
 1-init_dog.c:Initialize a variable of type struct dog
 2-print_dog.c:Print the items of a struct do variable
+4-new_dog.c:Create a new struct dog variable
