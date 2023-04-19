98be83113fa88decd587ba4940361e8378b220b5
Kareem-Ataam <kareem.ataam12345@gmail.com>
2023-04-19 18:42:21 +0200

    Print a name using function pointer

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,6 @@
 #ifndef _HEADER_H
 #define _HEADER_H
+#include<stdlib.h>
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
