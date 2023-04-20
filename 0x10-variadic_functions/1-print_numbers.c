3ab629fc38608a8e4e1d7ee66e1c99f3d7fbc3dc
Kareem-Ataam <kareem.ataam12345@gmail.com>
2023-04-20 20:52:49 +0200

    Print the numbers passed to a function

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,30 @@
+#include<stdarg.h>
+#include<stdlib.h>
+#include"variadic_functions.h"
+/**
+ * print_numbers - Print the numbers passed to the function
+ * @separator: The separator used among the numbers
+ * @n: Number of numbers to be printed
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	size_t i;
+	va_list ptr;
+
+	va_start(ptr, n);
+	for (i = 0; i < n; i++)
+	{
+		if (i == n - 1)
+		{
+			printf("%d\n", va_arg(ptr, int));
+			break;
+		}
+		printf("%d", va_arg(ptr, int));
+		if (!separator)
+		{
+			continue;
+		}
+		printf("%s", separator);
+	}
+	va_end(ptr);
+}
--- 0x10-variadic_functions/README.md
+++ 0x10-variadic_functions/README.md
@@ -1,1 +1,2 @@
 0-sum_them_all.c:Get the sum of all the parameters passed to the function
+1-print_numbers.c:Print the numbers passed to a function
