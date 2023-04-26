b3c34416fed6398883e716e7748ac42821ae1239
Kareem-Ataam <kareem.ataam12345@gmail.com>
2023-04-26 18:51:51 +0200

    Check if a string is palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,26 +1,34 @@
 #include<string.h>
 #include"main.h"
 /**
+ * comp - compares the characters of a string
+ * @c: the string
+ * @num1: first pointer
+ * @num2: Second pointer
+ * Return: Result of compariosn
+ */
+int comp(char *c, int num1, int num2)
+{
+	if (*(c + num1) == *(c + num2))
+	{
+		if (num1 == num2 || num1 == num2 + 1)
+		{
+			return (1);
+		}
+		return (0 + comp(c, num1 + 1, num2 - 1));
+	}
+	return (0);
+}
+
+/**
  * is_palindrome - Check if a string is palindrome
  * @s: The string to be checked
  * Return: (1) if palindrome (0) otherwise
  */
 int is_palindrome(char *s)
 {
-	int len;
-
-	len = strlen(s);
-	if (len <= 1)
-	{
+	if (*s == '\0')
 		return (1);
-	}
-	else if (s[0] != s[len - 1])
-	{
-		return (0);
-	}
-	else
-	{
-		s[len - 1] = '\0';
-		return (is_palindrome(&s[1]));
-	}
+	return (comp(s, 0, strlen(s) - 1));
+
 }
