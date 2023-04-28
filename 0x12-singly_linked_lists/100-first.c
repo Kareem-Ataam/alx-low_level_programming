b181aa84ad6531e31e0ce4f61a11f63d253d8770
Kareem-Ataam <kareem.ataam12345@gmail.com>
2023-04-28 03:20:45 +0300

    Print a message before executing the main function

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void print_before_main(void) __attribute__((constructor));
+/**
+ * print_before_main - Print a message before executing main
+ */
+void print_before_main(void)
+{
+	printf("You're beat! and yet, you must allow,
+			\nI bore my house upon my back!\n");
+}
--- 0x12-singly_linked_lists/README.md
+++ 0x12-singly_linked_lists/README.md
@@ -3,3 +3,4 @@
 2-add_node.c:Add a node before the first node of a linked list
 3-add_node_end.c:add a node to the end of a linked list
 4-free_list.c:Free all the nodes of a linked list
+100-first.c:Print a message before executing the main function
